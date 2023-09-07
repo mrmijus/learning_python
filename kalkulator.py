343a06e3f78eaa5fd674d230347ca80ada698cfd
misocernak <40561437+misocernak@users.noreply.github.com>
2023-09-07 23:10:07 +0200

    Update kalkulator.py
    Changes regarding the comments from Milos
    Couldn't use simpleeval() function - it was unclear to me how to call it, should I import the module to be able to use simpleeval, tried with import ast and from simpleeval import simple_eval, nothing happens

--- kalkulator.py
+++ kalkulator.py
@@ -3,20 +3,13 @@ OPERATIONS = ["+", "-", "*", "/", "%"]
 def check_operation(math_operation: str) -> bool:
     if math_operation not in OPERATIONS:
         return True
-    else:
-        return False
+    return False
+
 
 def calculate(num_a, num_b, operation):
     result = eval(str(num_a) + operation + str(num_b))
-    # if operation == OPERATIONS[0]:
-    #     result = num_a + num_b
-    # elif operation == OPERATIONS[1]:
-    #     result = num_a - num_b
-    # elif operation == OPERATIONS[2]:
-    #     result = num_a * num_b
-    # else:
-    #     result = num_a / num_b
     print(result)
+
 
 def main():
     num_a = int(input("First number: "))
@@ -28,4 +21,6 @@ def main():
 
     calculate(num_a, num_b, operator)
 
-main()
+
+if __name__ == "__main__":
+    main()
